c61d68ae33a1045027369baec9e33410473b151f
Sebastiaan-Willem Kesteloot <sebastiaanwillem.kesteloot.v5118@student.hogent.be>
2016-02-10 18:24:05 +0100

    Klasse test toegevoegd om commit te testen

---
+++ Projecten2.NET/Models/CommitTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projecten2.NET.Models
+{
+    public class CommitTest
+    {
+        //Dit is een test
+    }
+}
