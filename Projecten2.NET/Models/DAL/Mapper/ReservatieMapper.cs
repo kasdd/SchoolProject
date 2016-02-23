95e3bc5bfc2b903e3031d03588da0ca6d014cc02
Eva Ottenbourgs <eva.ottenbourgs.t4261@student.hogent.be>
2016-02-23 23:43:14 +0100

    Revert "ForeignKey in de ReservatieMapper"
    This reverts commit 6b40fead1472a11594a0f08ef20cd4e5a5b2c0dd.

--- Projecten2.NET/Models/DAL/Mapper/ReservatieMapper.cs
+++ Projecten2.NET/Models/DAL/Mapper/ReservatieMapper.cs
@@ -12,14 +12,9 @@ namespace Projecten2.NET.Models.DAL.Mapper
         {
             ToTable("Reservatie");
 
-            this.HasKey(r => r.ReservatieId);
+            HasKey(r => r.ReservatieId);
 
-            //HasRequired(r => r.Materialen).WithMany().Map(r => r.MapKey("ReservatieId"));
-
-            HasRequired(r => r.Materialen)
-              .WithMany()
-              .HasForeignKey(r => r.ReservatieId); //Nog incorrect maar zou ForeignKey kunnen zijn volgens andere mensen hun problemen op stackoverflow.
-            //Nu wel multiplicity probleem.... Gewoon gepushed om te tonen
+            HasRequired(r => r.Materialen).WithMany().Map(r => r.MapKey("ReservatieId"));
         }
     }
 }
