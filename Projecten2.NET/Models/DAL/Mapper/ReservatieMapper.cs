6b40fead1472a11594a0f08ef20cd4e5a5b2c0dd
Eva Ottenbourgs <eva.ottenbourgs.t4261@student.hogent.be>
2016-02-23 08:36:11 +0100

    ForeignKey in de ReservatieMapper

--- Projecten2.NET/Models/DAL/Mapper/ReservatieMapper.cs
+++ Projecten2.NET/Models/DAL/Mapper/ReservatieMapper.cs
@@ -12,9 +12,14 @@ namespace Projecten2.NET.Models.DAL.Mapper
         {
             ToTable("Reservatie");
 
-            HasKey(r => r.ReservatieId);
+            this.HasKey(r => r.ReservatieId);
 
-            HasRequired(r => r.Materialen).WithMany().Map(r => r.MapKey("ReservatieId"));
+            //HasRequired(r => r.Materialen).WithMany().Map(r => r.MapKey("ReservatieId"));
+
+            HasRequired(r => r.Materialen)
+              .WithMany()
+              .HasForeignKey(r => r.ReservatieId); //Nog incorrect maar zou ForeignKey kunnen zijn volgens andere mensen hun problemen op stackoverflow.
+            //Nu wel multiplicity probleem.... Gewoon gepushed om te tonen
         }
     }
 }
