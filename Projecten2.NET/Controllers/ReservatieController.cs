ba3262f9696cc32393a50436198b737f4953c4fa
Robin Van_den_Broeck <robin.van.den.broeck@telenet.be>
2016-03-03 09:39:06 +0100

    UC4 visueel wel alwat

--- Projecten2.NET/Controllers/ReservatieController.cs
+++ Projecten2.NET/Controllers/ReservatieController.cs
@@ -7,60 +7,58 @@ using System.Web.Mvc;
 
 namespace Projecten2.NET.Controllers
 {
+    [Authorize]
     public class ReservatieController : Controller
     {
 
-        public class VerlanglijstController : Controller
+        private IMateriaalRepository materiaalRepository;
+        private IGebruikerRepository gebruikersRepository;
+
+        public ReservatieController(IMateriaalRepository materiaalRepository, IGebruikerRepository gebruikerRepository)
         {
-            private IMateriaalRepository materiaalRepository;
-            private IGebruikerRepository gebruikersRepository;
+            this.materiaalRepository = materiaalRepository;
+            this.gebruikersRepository = gebruikerRepository;
+        }
 
-            public VerlanglijstController(IMateriaalRepository materiaalRepository, IGebruikerRepository gebruikerRepository)
+        // GET: Reservatie
+        public ActionResult Index(Gebruiker gebruiker)
+        {
+            DateTime startdatum = new DateTime();
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
             {
-                this.materiaalRepository = materiaalRepository;
-                this.gebruikersRepository = gebruikerRepository;
-            }
-
-            // GET: Reservatie
-            public ActionResult Index(Gebruiker gebruiker)
-            {
-                DateTime startdatum = new DateTime();
-                if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+                if (DateTime.Now.TimeOfDay.Hours < 17 /*Convert.ToDateTime("05:00:00 PM")*/)
                 {
-                    if (DateTime.Now.TimeOfDay.Hours < 17 /*Convert.ToDateTime("05:00:00 PM")*/)
-                    {
-                        startdatum = GetNextWeekday(DateTime.Today, DayOfWeek.Monday);
-                    }
-                    else
-                    {
-                        startdatum = GetNextWeekday(DateTime.Today.AddDays(7), DayOfWeek.Monday);
-                    }
-                }
-                else if (DateTime.Now.DayOfWeek < DayOfWeek.Friday)
-                {
-                    startdatum = GetNextWeekday(DateTime.Today.AddDays(1), DayOfWeek.Monday);
+                    startdatum = GetNextWeekday(DateTime.Today, DayOfWeek.Monday);
                 }
                 else
                 {
                     startdatum = GetNextWeekday(DateTime.Today.AddDays(7), DayOfWeek.Monday);
                 }
-
-
-                if (gebruiker.Verlanglijst.Materialen.Count == 0)
-                {
-                    return View("LegeLijst");
-                }
-                ViewBag.Total = gebruiker.Verlanglijst.Materialen.Count;
-                ViewBag.Startdatum = startdatum;
-                return View(gebruiker.Verlanglijst.Materialen);
-
             }
-
-            public static DateTime GetNextWeekday(DateTime vandaag, DayOfWeek verwachteDag)
+            else if (DateTime.Now.DayOfWeek < DayOfWeek.Friday)
             {
-                int daysToAdd = ((int)verwachteDag - (int)vandaag.DayOfWeek + 7) % 7;
-                return vandaag.AddDays(daysToAdd);
+                startdatum = GetNextWeekday(DateTime.Today.AddDays(1), DayOfWeek.Monday);
             }
+            else
+            {
+                startdatum = GetNextWeekday(DateTime.Today.AddDays(7), DayOfWeek.Monday);
+            }
+
+
+            if (gebruiker.Verlanglijst.Materialen.Count == 0)
+            {
+                return View("LegeLijst");
+            }
+            ViewBag.Total = gebruiker.Verlanglijst.Materialen.Count;
+            ViewBag.Startdatum = startdatum;
+            return View(gebruiker.Verlanglijst.Materialen);
+
+        }
+
+        public static DateTime GetNextWeekday(DateTime vandaag, DayOfWeek verwachteDag)
+        {
+            int daysToAdd = ((int)verwachteDag - (int)vandaag.DayOfWeek + 7) % 7;
+            return vandaag.AddDays(daysToAdd);
         }
     }
 }
