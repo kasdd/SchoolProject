7386c8ed404fb11210603d3989cc1377b75aca7d
kasdd08 <kas_dedurpel@hotmai.com>
2016-03-15 11:15:16 +0100

    javascript

--- Projecten2.NET/Scripts/nieuweReservatie.js
+++ Projecten2.NET/Scripts/nieuweReservatie.js
@@ -64,18 +64,4 @@ $(document).ready(function () {
         initStatus: 'Kies een datum', isRTL: false
     };
     $.datepicker.setDefaults($.datepicker.regional['nl']);
-
-    $.validator.addMethod('date', function (value, element) {
-        if (this.optional(element)) {
-            return true;
-        }
-        var valid = true;
-        try {
-            $.datepicker.parseDate('dd/mm/yy', value);
-        }
-        catch (err) {
-            valid = false;
-        }
-        return valid;
-    });
 });
